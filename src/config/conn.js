e7c6a98d5e07e5a55555eb823dd76fdb8624df14
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2024-06-24 06:44:16 -0300

    Update conn.js

--- src/config/conn.js
+++ src/config/conn.js
@@ -7,8 +7,8 @@ async function main() {
    const password = process.env.MONGODB_PASSWORD;
    const user = process.env.MONGODB_USER;
    mongoose.set('strictQuery', true);
-   await mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.nayh0ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
    console.log('Conectado com o banco de dados com sucesso!');
+   await mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.nayh0ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
   } catch (error) {
    console.log(error);
   }
